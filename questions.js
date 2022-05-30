e5da65cb8513c9eea296be2923f1f45c799a061f
Lakshmi <mlakshmi.76@gmail.com>
2022-05-30 19:01:26 -0400

    initial skeleton of HTML,css, js

---
+++ questions.js
@@ -0,0 +1,134 @@
+//got the questions from https://www.interviewbit.com/javascript-mcq/
+
+var myQuestions = [
+  {
+    question: "Javascript is an _______ language?",
+    answers: {
+      a: "Object-Oriented",
+      b: "Procedural",
+      c: "Object-Based",
+    },
+
+    answer: "a",
+  },
+  {
+    question: "Which of the following keywords is used to define a variable in Javascript?",
+    answers: {
+      a: "var",
+      b: "let",
+      c: "Both A and B",
+    },
+
+    answer: "c",
+  },
+  {
+    question: "Which of the following methods is used to access HTML elements using Javascript?",
+    answers: {
+      a: "getElementbyId()",
+      b: "getElementbyClassName()",
+      c: "Both A and B",
+    },
+
+    answer: "c",
+  },
+  {
+    question: "Upon encountering empty statements, what does the Javascript Interpreter do?",
+    answers: {
+      a: "Throws an error",
+      b: "Ignores the statements",
+      c: "Gives a warning",
+    },
+
+    answer: "b",
+  },
+  {
+    question: "Which of the following methods can be used to display data in some form using Javascript?",
+    answers: {
+      a: "document.write()",
+      b: "console.log()",
+      c: "All of the above",
+    },
+
+    answer: "c",
+  },
+  {
+    question: "How can a datatype be declared to be a constant type?",
+    answers: {
+      a: "const",
+      b: "var",
+      c: "let",
+    },
+
+    answer: "a",
+  },
+  {
+    question: "What keyword is used to check whether a given property is valid or not?",
+    answers: {
+      a: "in",
+      b: "is in",
+      c: "exists",
+    },
+
+    answer: "a",
+  },
+  {
+    question: "When an operators value is NULL, the typeof returned by the unary operator is:",
+    answers: {
+      a: "Boolean",
+      b: "Undefined",
+      c: "Object",
+    },
+
+    answer: "c",
+  },
+  {
+    question: "What does the Javascript “debugger” statement do?",
+    answers: {
+      a: "It will debug the errors in the program at runtime",
+      b: "It acts as a breakpoint in a program",
+      c: "All of the above",
+    },
+
+    answer: "b",
+  },
+  {
+    question: "Which function is used to serialize an object into a JSON string in Javascript?",
+    answers: {
+      a: "stringify()",
+      b: "parse()",
+      c: "convert()",
+    },
+
+    answer: "a",
+  },
+  {
+    question: "How to stop an interval timer in Javascript?",
+    answers: {
+      a: "clearInterval",
+      b: "clearTimer",
+      c: "intervalOver",
+    },
+
+    answer: "a",
+  },
+  {
+    question: "What keyword is used to declare an asynchronous function in Javascript?",
+    answers: {
+      a: "async",
+      b: "await",
+      c: "setTimeOut",
+    },
+
+    answer: "a",
+  },
+  {
+    question: "How do we write a comment in javascript?",
+    answers: {
+      a: "/* */",
+      b: "//",
+      c: "#",
+    },
+
+    answer: "b",
+  }
+];
