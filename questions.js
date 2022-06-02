e6e4879c8c785372e75f6abaf601da6fac2d94ad
Lakshmi <mlakshmi.76@gmail.com>
2022-06-01 22:12:51 -0400

    deleted timer and added the logic in Html, Added styles, Modified the questions.js

--- questions.js
+++ questions.js
@@ -1,134 +1,182 @@
 //got the questions from https://www.interviewbit.com/javascript-mcq/
 
-var myQuestions = [
+const questions=[
   {
-    question: "Javascript is an _______ language?",
-    answers: {
-      a: "Object-Oriented",
-      b: "Procedural",
-      c: "Object-Based",
+      question: 'Javascript is an _______ language?',
+      answers: [
+          {text:'Object-Oriented', correct: true},
+          {text:'Procedural', correct: false},
+          {text:'Object-Based', correct: false},
+          {text:'None of the above', correct: false},
+      ]
+     
+      },
+  
+    {
+      question: 'Which of the following keywords is used to define a variable in Javascript?',
+      answers: [
+          {text:'var', correct: false},
+          {text:'let', correct: false},
+          {text:'var and let', correct: true},
+          {text:'none', correct: false},
+      ]
+      
     },
-
-    answer: "a",
+    {
+      question: 'Which of the following methods is used to access HTML elements using Javascript?',
+      answers:
+      [
+          {text:'getElementbyId()', correct: false},
+          {text:'getElementbyClassName()', correct: false},
+          {text:'getElementbyId() and getElementbyClassName()', correct: true},
+          {text:'None of the above', correct: false},
+      ] 
   },
-  {
-    question: "Which of the following keywords is used to define a variable in Javascript?",
-    answers: {
-      a: "var",
-      b: "let",
-      c: "Both A and B",
-    },
 
-    answer: "c",
-  },
-  {
-    question: "Which of the following methods is used to access HTML elements using Javascript?",
-    answers: {
-      a: "getElementbyId()",
-      b: "getElementbyClassName()",
-      c: "Both A and B",
-    },
+    {
+      question: 'Upon encountering empty statements, what does the Javascript Interpreter do?',
+      answers:
+      [
+          {text:'Throws an error', correct: false},
+          {text:'Ignores the statements', correct: true},
+          {text:'Gives a warning', correct: false},
+          {text:'None of the above', correct: false},
+      ] }
+]
 
-    answer: "c",
-  },
-  {
-    question: "Upon encountering empty statements, what does the Javascript Interpreter do?",
-    answers: {
-      a: "Throws an error",
-      b: "Ignores the statements",
-      c: "Gives a warning",
-    },
 
-    answer: "b",
-  },
-  {
-    question: "Which of the following methods can be used to display data in some form using Javascript?",
-    answers: {
-      a: "document.write()",
-      b: "console.log()",
-      c: "All of the above",
-    },
 
-    answer: "c",
-  },
-  {
-    question: "How can a datatype be declared to be a constant type?",
-    answers: {
-      a: "const",
-      b: "var",
-      c: "let",
-    },
 
-    answer: "a",
-  },
-  {
-    question: "What keyword is used to check whether a given property is valid or not?",
-    answers: {
-      a: "in",
-      b: "is in",
-      c: "exists",
-    },
 
-    answer: "a",
-  },
-  {
-    question: "When an operators value is NULL, the typeof returned by the unary operator is:",
-    answers: {
-      a: "Boolean",
-      b: "Undefined",
-      c: "Object",
-    },
+// var myQuestions = [
+//   {
+//     question: "Javascript is an _______ language?",
+//     answers: {
+//       a: "Object-Oriented",
+//       b: "Procedural",
+//       c: "Object-Based",
+//     },
 
-    answer: "c",
-  },
-  {
-    question: "What does the Javascript “debugger” statement do?",
-    answers: {
-      a: "It will debug the errors in the program at runtime",
-      b: "It acts as a breakpoint in a program",
-      c: "All of the above",
-    },
+//     answer: "a",
+//   },
+//   {
+//     question: "Which of the following keywords is used to define a variable in Javascript?",
+//     answers: {
+//       a: "var",
+//       b: "let",
+//       c: "Both A and B",
+//     },
 
-    answer: "b",
-  },
-  {
-    question: "Which function is used to serialize an object into a JSON string in Javascript?",
-    answers: {
-      a: "stringify()",
-      b: "parse()",
-      c: "convert()",
-    },
+//     answer: "c",
+//   },
+//   {
+//     question: "Which of the following methods is used to access HTML elements using Javascript?",
+//     answers: {
+//       a: "getElementbyId()",
+//       b: "getElementbyClassName()",
+//       c: "Both A and B",
+//     },
 
-    answer: "a",
-  },
-  {
-    question: "How to stop an interval timer in Javascript?",
-    answers: {
-      a: "clearInterval",
-      b: "clearTimer",
-      c: "intervalOver",
-    },
+//     answer: "c",
+//   },
+//   {
+//     question: "Upon encountering empty statements, what does the Javascript Interpreter do?",
+//     answers: {
+//       a: "Throws an error",
+//       b: "Ignores the statements",
+//       c: "Gives a warning",
+//     },
 
-    answer: "a",
-  },
-  {
-    question: "What keyword is used to declare an asynchronous function in Javascript?",
-    answers: {
-      a: "async",
-      b: "await",
-      c: "setTimeOut",
-    },
+//     answer: "b",
+//   },
+//   {
+//     question: "Which of the following methods can be used to display data in some form using Javascript?",
+//     answers: {
+//       a: "document.write()",
+//       b: "console.log()",
+//       c: "All of the above",
+//     },
 
-    answer: "a",
-  },
-  {
-    question: "How do we write a comment in javascript?",
-    answers: {
-      a: "/* */",
-      b: "//",
-      c: "#",
-    },
+//     answer: "c",
+//   },
+//   {
+//     question: "How can a datatype be declared to be a constant type?",
+//     answers: {
+//       a: "const",
+//       b: "var",
+//       c: "let",
+//     },
 
-    answer: "b",
-  }
-];
+//     answer: "a",
+//   },
+//   {
+//     question: "What keyword is used to check whether a given property is valid or not?",
+//     answers: {
+//       a: "in",
+//       b: "is in",
+//       c: "exists",
+//     },
+
+//     answer: "a",
+//   },
+//   {
+//     question: "When an operators value is NULL, the typeof returned by the unary operator is:",
+//     answers: {
+//       a: "Boolean",
+//       b: "Undefined",
+//       c: "Object",
+//     },
+
+//     answer: "c",
+//   },
+//   {
+//     question: "What does the Javascript “debugger” statement do?",
+//     answers: {
+//       a: "It will debug the errors in the program at runtime",
+//       b: "It acts as a breakpoint in a program",
+//       c: "All of the above",
+//     },
+
+//     answer: "b",
+//   },
+//   {
+//     question: "Which function is used to serialize an object into a JSON string in Javascript?",
+//     answers: {
+//       a: "stringify()",
+//       b: "parse()",
+//       c: "convert()",
+//     },
+
+//     answer: "a",
+//   },
+//   {
+//     question: "How to stop an interval timer in Javascript?",
+//     answers: {
+//       a: "clearInterval",
+//       b: "clearTimer",
+//       c: "intervalOver",
+//     },
+
+//     answer: "a",
+//   },
+//   {
+//     question: "What keyword is used to declare an asynchronous function in Javascript?",
+//     answers: {
+//       a: "async",
+//       b: "await",
+//       c: "setTimeOut",
+//     },
+
+//     answer: "a",
+//   },
+//   {
+//     question: "How do we write a comment in javascript?",
+//     answers: {
+//       a: "/* */",
+//       b: "//",
+//       c: "#",
+//     },
+
+//     answer: "b",
+//   }
+// ];
--- script.js
+++ script.js
@@ -1,61 +1,172 @@
-const startButton = document.getElementById('start-btn')
-const nextButton = document.getElementById('next-btn')
 
-const questionContainerElement = document.getElementById('question-container')
-const questionElement = document.getElementById('question')
-const answerButtonsElement = document.getElementById('answer-buttons')
+const _strtBtn = document.getElementById('start-btn')
+const _nxtBtn = document.getElementById('next-btn')
 
-startButton.addEventListener('click',start)
+const _qstContainerElement = document.getElementById('question-container')
+const _qstElement = document.getElementById('question')
+const _ansBtnElement = document.getElementById('answer-buttons')
+var _timerObj= document.getElementById("time");
+
+//let shuffledQuestions, _curIndex;
+var _curIndex=0
+let _quizScore = 0;
+var _totalQuestions=questions.length;
+var _time=_totalQuestions*5
+_strtBtn.addEventListener('click',()=>{
+    if (_strtBtn.innerText=="Submit"){
+        ShowScore()
+    }
+    else{
+        start()
+    }
+})
+
+_nxtBtn.addEventListener('click',()=>{
+ 
+    _curIndex++
+    navigate()
+})
+//when Submit is clicked hide questions section and show the score area
+function ShowScore(){
+    _qstContainerElement.setAttribute("class", "hide");
+    var endScreenEl = document.getElementById("end-screen");
+    endScreenEl.removeAttribute("class");
+    // show final score
+    var finalScoreEl = document.getElementById("final-score");
+    finalScoreEl.textContent = _quizScore;
+
+}
+//Timer callback
+function TimerCallBack() {
+
+    _time--;
+    _timerObj.textContent = _time;
+    //alert(_time)
+    // check if user ran out of time
+    if (_time <= 0) {
+    //   alert('time is less than 0')  
+      //StopQuiz();
+    }
+}
+
+//Start Function triggered by start click
+function start(){
+    //hide the start button
+    _strtBtn.classList.add('hide')
+     _curIndex=0;
+    _qstContainerElement.classList.remove('hide')
+    //start timer
+    //init the timer with callback
+    _timerId = setInterval(TimerCallBack, 1000);
+
+    //show starting time
+    _timerObj.textContent = _time;
+    
+    //Logic to Show the question one by one 
+    navigate()
 
 
+    _quizScore=0
+}
+//Triggered when the end of question or timer completes
+function StopQuiz(){
+    // alert('end of quiz')
+    var endScreenEl = document.getElementById("end-screen");
+    endScreenEl.removeAttribute("class");
 
+    // show final score
+    var finalScoreEl = document.getElementById("final-score");
+    finalScoreEl.textContent = _quizScore;
+    _qstContainerElement.classList.remove('hide')
+}
 
+//Reset screen state and show the question one by one by curIndex
+function navigate(){
 
+    if (_curIndex > _totalQuestions)
+         alert('calling stop quiz')
+        //StopQuiz()
 
+    resetState();
+    // alert('before show quesion')
+    showQuestion(questions[_curIndex])
+}
+//Bind the Question collections data to user interface
+function showQuestion(aQuestion){
+        _qstElement.innerText= aQuestion.question;
+        aQuestion.answers.forEach((answer)=>{
+        const fBtn=document.createElement('button')
+        fBtn.innerText=answer.text;
+        fBtn.classList.add('btn')
+ 
+        if(answer.correct){
+            fBtn.dataset.correct=answer.correct
+        }
+        else{
+            fBtn.dataset.correct=false
+        }
+        // alert('hooking selectanser')
+        fBtn.addEventListener('click',selectAnswer)
+        _ansBtnElement.appendChild(fBtn)
+    })
+}
+//hide and button state 
+function resetState(){
+     clearStatusClass(document.body)
+    _nxtBtn.classList.add('hide')
+    while(_ansBtnElement.firstChild){
+        _ansBtnElement.removeChild(_ansBtnElement.firstChild)
+    }
+}
 
+//when correct ansert is selected track the score and when reached the end of question
+//show start button as Submit button
+function selectAnswer(e){
+    const selectedButton=e.target
+    const bCorrect = selectedButton.dataset.correct
+  
+    
+    setStatusClass(document.body, bCorrect)
+  
+    Array.from(_ansBtnElement.children).forEach((button)=>{
 
-const questions=[
-   {
-       question: 'Javascript is an _______ language?',
-       answers: [
-           {text:'Object-Oriented', correct: true},
-           {text:'Procedural', correct: false},
-           {text:'Object-Based', correct: false},
-           {text:'None of the above', correct: false},
-       ]
-      
-       },
-   
-     {
-       question: 'Which of the following keywords is used to define a variable in Javascript?',
-       answers: [
-           {text:'var', correct: false},
-           {text:'let', correct: false},
-           {text:'var and let', correct: true},
-           {text:'none', correct: false},
-       ]
-       
-     },
-     {
-       question: 'Which of the following methods is used to access HTML elements using Javascript?',
-       answers:
-       [
-           {text:'getElementbyId()', correct: false},
-           {text:'getElementbyClassName()', correct: false},
-           {text:'getElementbyId() and getElementbyClassName()', correct: true},
-           {text:'None of the above', correct: false},
-       ] 
-   },
+        setStatusClass(button,button.dataset.correct)
+    })
 
-     {
-       question: 'Upon encountering empty statements, what does the Javascript Interpreter do?',
-       answers:
-       [
-           {text:'Throws an error', correct: false},
-           {text:'Ignores the statements', correct: true},
-           {text:'Gives a warning', correct: false},
-           {text:'None of the above', correct: false},
-       ] }
-]
+    if(_totalQuestions>_curIndex +1){
+        _nxtBtn.classList.remove("hide")
+    }
+    else{
+        _strtBtn.innerText="Submit"
+        _strtBtn.classList.remove("hide")
+    }
+  
+    // alert('select buton check')    
+    if(selectedButton.dataset==bCorrect){
+        _quizScore++
+        // alert(_quizScore)
+    }
+
+    document.getElementById('right-answers').innerText=_quizScore
+     //window.alert('end of select answer')
+}
+
+//color change logic---need to work on
+function setStatusClass(element,correct){
+   // clearStatusClass(element)
+    if(correct){
+        element.classList.add("correct")
+    }else{
+        element.classList.add("wrong")
+    }
+}
+
+//change the class of the element
+function clearStatusClass(element){
+    // window.alert(element)
+    // window.alert(element.classList)
+    element.classList.remove('correct')
+    element.classList.remove('wrong')
+}
 
 
